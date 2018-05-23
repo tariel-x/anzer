8f6c7a0adf0e55b69e96d383b63a636dab3f96d1
Nikita Gerasimov <tariel-x@ya.ru>
2018-05-24 01:01:26 +0300

    add parsing variables

--- interpeter/listener.go
+++ interpeter/listener.go
@@ -2,8 +2,8 @@ package interpeter
 
 import (
 	"github.com/tariel-x/anzer/parser"
-	"github.com/antlr/antlr4/runtime/Go/antlr"
 	"fmt"
+	"github.com/antlr/antlr4/runtime/Go/antlr"
 )
 
 type Listener struct{
@@ -14,6 +14,40 @@ func NewListener() *Listener {
 	return new(Listener)
 }
 
-func (l *Listener) EnterEveryRule(ctx antlr.ParserRuleContext) {
-	fmt.Println(ctx.GetText())
+//func (l *Listener) EnterEveryRule(ctx antlr.ParserRuleContext) {
+//	fmt.Println(ctx.GetText())
+//}
+
+func (l *Listener) EnterDataSig(ctx *parser.DataSigContext) {
+	fmt.Printf("---enter data sig %s\n", ctx.GetText())
+
+	name := ""
+	val := ""
+	for _, child :=  range ctx.GetChildren() {
+		payload := child.GetPayload()
+		switch t := payload.(type) {
+		case *antlr.CommonToken:
+			fmt.Printf("%T\n", payload)
+			fmt.Printf("comm tkn\n")
+			fmt.Printf("string: %s\n", t.String())
+		case *antlr.BaseParserRuleContext:
+			fmt.Printf("%T\n", payload)
+			fmt.Printf("ctx\n")
+
+			fmt.Printf("rule index %d ", t.RuleIndex)
+			fmt.Printf("text: %s\n", t.GetText())
+			if t.RuleIndex == 6 {
+				name = t.GetText()
+			}
+			if t.RuleIndex == 8 {
+				val = t.GetText()
+			}
+		default:
+			_ = t
+			fmt.Printf("unkn\n")
+			fmt.Printf("%T\n", payload)
+		}
+	}
+
+	fmt.Printf("Name: %s val: %s\n", name, val)
 }
