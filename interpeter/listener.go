8b98d6e3a87d48c7a705f176d54c60a33159c8d5
Nikita Gerasimov <n.gerasimov@rambler-co.ru>
2018-05-25 14:56:15 +0300

    modify listener according to grammar

--- interpeter/listener.go
+++ interpeter/listener.go
@@ -1,8 +1,8 @@
 package interpeter
 
 import (
-	"fmt"
-	"github.com/antlr/antlr4/runtime/Go/antlr"
+	//"fmt"
+	//"github.com/antlr/antlr4/runtime/Go/antlr"
 	"github.com/tariel-x/anzer/parser"
 )
 
@@ -21,19 +21,19 @@ func NewListener() *Listener {
 	return l
 }
 
-func (l *Listener) EnterEveryRule(ctx antlr.ParserRuleContext) {
+/* func (l *Listener) EnterEveryRule(ctx antlr.ParserRuleContext) {
 	fmt.Println(ctx.GetText())
-}
+} */
 
-/*func (l *Listener) EnterDataSig(ctx *parser.DataSigContext) {
-	name := ctx.DataNameId().GetText()
-	val := ctx.Json().GetText()
+func (l *Listener) EnterDataSig(ctx *parser.DataSigContext) {
+	name := ctx.DATA_NAME_ID().GetText()
+	val := ctx.DataDefinition().GetText()
 	l.Types[name] = NewBaseType(name, val)
 }
 
 func (l *Listener) EnterFuncSig(ctx *parser.FuncSigContext) {
-	name := ctx.FuncNameId().GetText()
+	name := ctx.FUNC_NAME_ID().GetText()
 	arg := ctx.DataName(0).GetText()
 	ret := ctx.DataName(1).GetText()
 	l.Funcs[name] = NewBaseFunc(name, arg, ret)
-}*/
+}
