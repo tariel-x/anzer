5b3a19d8db104e61ef3c8220a0c5171834184a49
Nikita Gerasimov <tariel-x@ya.ru>
2018-06-13 23:52:19 +0300

    check array subitems

--- types/checker/array.go
+++ types/checker/array.go
@@ -4,24 +4,77 @@ import "github.com/tariel-x/anzer/types"
 
 func checkArray(parent, child types.JsonSchema) TypesIdentity {
 
-	if parent.MaxItems != child.MaxItems {
-		return TypesNotEqual
-	}
-	if parent.MinItems != child.MinItems {
-		return TypesNotEqual
-	}
-	if parent.Items != child.Items {
-		return TypesNotEqual
-	}
-	if parent.Contains != child.Contains {
-		return TypesNotEqual
-	}
-	if parent.AdditionalItems != child.AdditionalItems {
-		return TypesNotEqual
-	}
-	if parent.UniqueItems != child.UniqueItems {
+	var identity TypesIdentity
+	identity = TypesEqual
+
+	// maxItems
+	if parent.MaxItems != nil && child.MaxItems != nil && *parent.MaxItems != *child.MaxItems {
 		return TypesNotEqual
 	}
 
-	return TypesEqual
+	if parent.MaxItems == nil && child.MaxItems != nil {
+		identity = TypesSubtype
+	}
+
+	// minItems
+	if parent.MinItems != nil && child.MinItems != nil && *parent.MinItems != *child.MinItems {
+		return TypesNotEqual
+	}
+
+	if parent.MinItems == nil && child.MinItems != nil {
+		identity = TypesSubtype
+	}
+
+	// items
+	if parent.Items != nil && child.Items != nil{
+		switch Subtype(*parent.Items, *child.Items) {
+		case TypesNotEqual:
+			return TypesNotEqual
+		case TypesSubtype:
+			identity = TypesSubtype
+		}
+	}
+
+	if parent.Items == nil && child.Items != nil {
+		identity = TypesSubtype
+	}
+
+	// contains
+	if parent.Contains != nil && child.Contains != nil{
+		switch Subtype(*parent.Contains, *child.Contains) {
+		case TypesNotEqual:
+			return TypesNotEqual
+		case TypesSubtype:
+			identity = TypesSubtype
+		}
+	}
+
+	if parent.Contains == nil && child.Contains != nil {
+		identity = TypesSubtype
+	}
+
+	// additionalItems
+	if parent.AdditionalItems != nil && child.AdditionalItems != nil{
+		switch Subtype(*parent.AdditionalItems, *child.AdditionalItems) {
+		case TypesNotEqual:
+			return TypesNotEqual
+		case TypesSubtype:
+			identity = TypesSubtype
+		}
+	}
+
+	if parent.AdditionalItems == nil && child.AdditionalItems != nil {
+		identity = TypesSubtype
+	}
+
+	// uniqueItems
+	if parent.UniqueItems != nil && child.UniqueItems != nil && *parent.UniqueItems != *child.UniqueItems {
+		return TypesNotEqual
+	}
+
+	if parent.UniqueItems == nil && child.UniqueItems != nil {
+		identity = TypesSubtype
+	}
+
+	return identity
 }
--- types/checker/number.go
+++ types/checker/number.go
@@ -4,21 +4,53 @@ import "github.com/tariel-x/anzer/types"
 
 func checkNumber(parent, child types.JsonSchema) TypesIdentity {
 
-	if parent.Minimum != child.Minimum {
-		return TypesNotEqual
-	}
-	if parent.ExclusiveMinimum != child.ExclusiveMinimum {
-		return TypesNotEqual
-	}
-	if parent.Maximum != child.Maximum {
-		return TypesNotEqual
-	}
-	if parent.ExclusiveMaximum != child.ExclusiveMaximum {
-		return TypesNotEqual
-	}
-	if parent.MultipleOf != child.MultipleOf {
+	var identity TypesIdentity
+	identity = TypesEqual
+
+	// minimum
+	if parent.Minimum != nil && child.Minimum != nil && *parent.Minimum != *child.Minimum {
 		return TypesNotEqual
 	}
 
-	return TypesEqual
+	if parent.Minimum == nil && child.Minimum != nil {
+		identity = TypesSubtype
+	}
+
+	// exclusiveMinimum
+	if parent.ExclusiveMinimum != nil && child.ExclusiveMinimum != nil && *parent.ExclusiveMinimum != *child.ExclusiveMinimum {
+		return TypesNotEqual
+	}
+
+	if parent.ExclusiveMinimum == nil && child.ExclusiveMinimum != nil {
+		identity = TypesSubtype
+	}
+
+	// maximum
+	if parent.Maximum != nil && child.Maximum != nil && *parent.Maximum != *child.Maximum {
+		return TypesNotEqual
+	}
+
+	if parent.Maximum == nil && child.Maximum != nil {
+		identity = TypesSubtype
+	}
+
+	// exclusiveMaximum
+	if parent.ExclusiveMaximum != nil && child.ExclusiveMaximum != nil && *parent.ExclusiveMaximum != *child.ExclusiveMaximum {
+		return TypesNotEqual
+	}
+
+	if parent.ExclusiveMaximum == nil && child.ExclusiveMaximum != nil {
+		identity = TypesSubtype
+	}
+
+	// multipleOf
+	if parent.MultipleOf != nil && child.MultipleOf != nil && *parent.MultipleOf != *child.MultipleOf {
+		return TypesNotEqual
+	}
+
+	if parent.MultipleOf == nil && child.MultipleOf != nil {
+		identity = TypesSubtype
+	}
+
+	return identity
 }
