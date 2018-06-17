bd26d227b468837be3f53504ba7a832a56918e50
Nikita Gerasimov <tariel-x@ya.ru>
2018-06-17 17:58:07 +0300

    base for func processing

--- funcs/funcs.go
+++ funcs/funcs.go
@@ -1,7 +1,32 @@
 package funcs
 
-import "github.com/tariel-x/anzer/listener"
+import (
+	"github.com/tariel-x/anzer/listener"
+	"github.com/tariel-x/anzer/types"
+)
 
-func Resolve(funcs listener.Funcs) (*SystemGraph, error) {
+type FuncResolver struct {
+	RawFuncs listener.Funcs
+	Types    types.Types
+}
+
+func Resolve(funcs listener.Funcs, types types.Types) (*SystemGraph, error) {
+	fr := FuncResolver{
+		RawFuncs: funcs,
+		Types:    types,
+	}
+
+	return fr.ResolveAll()
+}
+
+func (fr *FuncResolver) ResolveAll() (*SystemGraph, error) {
+	for name, _ := range fr.RawFuncs {
+		fr.resolveRaw(name)
+	}
+
 	return nil, nil
+}
+
+func (fr *FuncResolver) resolveRaw(name string) {
+	// get raw func from the list and try to simplify it
 }
