68bd70c86004032bf852b2d467a1b65f7529f57f
Nikita Gerasimov <tariel-x@ya.ru>
2018-06-21 00:08:06 +0300

    foreach raw funcs starting main

--- funcs/funcs.go
+++ funcs/funcs.go
@@ -2,6 +2,8 @@ package funcs
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/tariel-x/anzer/listener"
 	"github.com/tariel-x/anzer/types"
 )
@@ -23,30 +25,79 @@ func Resolve(funcs listener.Funcs, types types.Types) (*SystemGraph, error) {
 }
 
 func (fr *FuncResolver) ResolveAll() (*SystemGraph, error) {
-	for name, _ := range fr.RawFuncs {
-		err := fr.resolveRaw(name)
-		if err != nil {
-			return nil, err
-		}
+	_, err := fr.resolveRaw("main")
+	if err != nil {
+		return nil, err
 	}
 
 	return nil, nil
 }
 
-func (fr *FuncResolver) resolveRaw(name string) error {
+func (fr *FuncResolver) resolveRaw(name string) ([]Service, error) {
 	rawDef, exists := fr.RawFuncs[name]
 	if !exists {
-		return fmt.Errorf("No such func %q in raw funcs list", name)
+		return nil, fmt.Errorf("No such func %q in raw funcs list", name)
 	}
+
+	services := []Service{}
 
 	if rawDef.Def == nil {
 		fmt.Printf("New empty func found")
-		fr.createLambda(rawDef)
+		l, err := fr.createLambda(rawDef)
+		if err != nil {
+			return nil, err
+		}
+		services = append(services, *l)
 	} else {
 		// iterate over child services or product child services and recursive add to graph
+		if rawDef.Def.ComposeTo != nil {
+			appended, err := fr.resolveCompose(rawDef.Def.ComposeTo)
+			if err != nil {
+				return nil, err
+			}
+			services = append(services, appended...)
+		}
 	}
 
-	return nil
+	return services, nil
+}
+
+func (fr *FuncResolver) resolveCompose(fb *listener.FuncBody) ([]Service, error) {
+	if fb.Name != nil {
+		return fr.resolveRaw(*fb.Name)
+	}
+	return nil, nil
+}
+
+func (fr *FuncResolver) resolveProduction(fb *listener.FuncBody) ([]Service, error) {
+	product := []Service{}
+	prodNames := []string{}
+	if fb.ProductEls != nil {
+		for _, pFunc := range fb.ProductEls {
+			productItem, err := fr.resolveRaw(*pFunc.Name)
+			prodNames = append(prodNames, *pFunc.Name)
+			if err != nil {
+				return nil, err
+			}
+			product = append(product, productItem...)
+		}
+	}
+	prodService, err := fr.createProduction(prodNames)
+	if err != nil {
+		return nil, err
+	}
+	product = append(product, *prodService)
+	return product, nil
+}
+
+func (fr *FuncResolver) createProduction(names []string) (*Service, error) {
+	name := strings.Join(names, "_")
+	s := Service{
+		Name:  name,
+		Index: 0,
+		Type:  TypeProduction,
+	}
+	return &s, nil
 }
 
 func (fr *FuncResolver) createLambda(rawDef listener.FuncDef) (*Service, error) {
