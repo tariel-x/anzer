367cf15a0d64021a1fa82400fdef0e701a9478f0
Nikita <tariel-x@ya.ru>
2019-01-16 21:06:15 +0300

    in-out types

--- internal/internal.go
+++ internal/internal.go
@@ -1,11 +1,14 @@
 package internal
 
 import (
+	"fmt"
 	"strings"
 )
 
 type Composable interface {
 	Definition() string
+	In() T
+	Out() T
 }
 
 type T interface{}
@@ -24,6 +27,20 @@ func (a Alias) Definition() string {
 	return strings.Join(definitions, " . ")
 }
 
+func (a Alias) In() T {
+	if len(a.Compose) == 0 {
+		panic(fmt.Errorf("Type %s is undefined", a.Name))
+	}
+	return a.Compose[0].In()
+}
+
+func (a Alias) Out() T {
+	if len(a.Compose) == 0 {
+		panic(fmt.Errorf("Type %s is undefined", a.Name))
+	}
+	return a.Compose[len(a.Compose)-1].Out()
+}
+
 type F struct {
 	Name    string
 	Link    string
@@ -33,6 +50,14 @@ type F struct {
 
 func (f F) Definition() string {
 	return f.Name
+}
+
+func (f F) In() T {
+	return f.TypeIn
+}
+
+func (f F) Out() T {
+	return f.TypeOut
 }
 
 type Applied []Composable
@@ -51,8 +76,24 @@ func (e EitherBind) Definition() string {
 	return ">>="
 }
 
+func (e EitherBind) In() T {
+	return nil
+}
+
+func (e EitherBind) Out() T {
+	return nil
+}
+
 type EitherReturn bool
 
 func (e EitherReturn) Definition() string {
 	return "return"
+}
+
+func (e EitherReturn) In() T {
+	return nil
+}
+
+func (e EitherReturn) Out() T {
+	return nil
 }
