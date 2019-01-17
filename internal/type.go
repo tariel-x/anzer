e2367aabf577f3c7f2180ba0727108402dd1eb5c
Nikita <tariel-x@ya.ru>
2019-01-18 00:14:23 +0300

    subtyping functions for constructors and basic types

--- internal/type.go
+++ internal/type.go
@@ -6,20 +6,57 @@ type T interface {
 	Parent(of T) bool
 }
 
-/*
-Alternatives to type Basic string:
-- type String string
-- type String Basic
-*/
-
-type Basic string
+type Basic int
 
 func (b Basic) Equal(to T) bool {
+	switch t := to.(type) {
+	case Basic:
+		return t == b
+	}
 	return false
+}
+
+func (b Basic) Parent(of T) bool {
+	return false
+}
+
+func (b Basic) Subtype(of T) bool {
+	return of.Parent(b)
 }
 
 type Constructor struct {
 	Operand  T
 	Type     string
 	Argument interface{}
+}
+
+func (c Constructor) Equal(to T) bool {
+	switch t := to.(type) {
+	case Constructor:
+		return c.Operand.Equal(c) &&
+			c.Argument == t.Argument &&
+			c.Type == t.Type
+	}
+	return false
+}
+
+func (c Constructor) Parent(of T) bool {
+	switch t := of.(type) {
+	case Constructor:
+		if c.Equal(t) {
+			return false
+		}
+	}
+	if c.Operand.Equal(of) {
+		return true
+	}
+	return false
+}
+
+func (c Constructor) Subtype(of T) bool {
+	return of.Parent(c)
+}
+
+type Complex struct {
+	Fields map[string]T
 }
