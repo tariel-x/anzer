7601e7b612b450c0395ce312f36f729b08ca1541
Nikita <tariel-x@ya.ru>
2019-02-03 19:01:10 +0300

    a bit rewrite example definition

--- main.go
+++ main.go
@@ -1,46 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/tariel-x/anzer/internal"
 )
 
 func main() {
-	fmt.Println("Hello")
-
-	fout := internal.TypeString
-	ain := fout
-	aout := internal.Construct(ain, internal.ConstructorMaxLength, 10)
-
 	s := internal.Alias{
+		Name: "composition1",
 		Compose: []internal.Composable{
 			internal.F{
 				Name:    "f",
-				TypeOut: fout,
+				Link:    "github.com/tariel-x/f",
+				TypeIn:  internal.TypeString,
+				TypeOut: internal.Construct(internal.TypeString, internal.ConstructorMaxLength, 10),
 			},
-			internal.Applied{
-				internal.F{
-					Name:    "a",
-					TypeIn:  ain,
-					TypeOut: aout,
-				},
-				internal.EitherBind(true),
-				internal.F{
-					Name: "b",
-				},
+			internal.F{
+				Name: "a",
+				Link: "github.com/tariel-x/a",
+			},
+			internal.F{
+				Name: "b",
+				Link: "github.com/tariel-x/b",
 			},
 		},
 	}
 
 	m := internal.Alias{
+		Name: "compisition2",
 		Compose: []internal.Composable{
-			s,
 			internal.F{
+				Link: "github.com/tariel-x/abc",
 				Name: "abc",
 			},
+			s,
 		},
 	}
+
+	out, err := json.MarshalIndent(m, "", "    ")
+	die(err)
+	fmt.Println(string(out))
 
 	//fmt.Printf("%#v\n", s)
 	fmt.Println(m.Definition())
