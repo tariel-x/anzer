8bf3a952847d4dd325d967f4a2fb978e120e4ac5
Nikita <tariel-x@ya.ru>
2019-02-04 20:54:02 +0300

    fix complex equal test

--- internal/internal_test.go
+++ internal/internal_test.go
@@ -40,3 +40,39 @@ func TestAliasValid(t *testing.T) {
 		t.Errorf("c must be valid, but: %s", err)
 	}
 }
+
+func TestAliasInvalid(t *testing.T) {
+	c := Alias{
+		Name: "c",
+		Compose: []Composable{
+			F{
+				Name:    "a",
+				TypeIn:  TypeString,
+				TypeOut: TypeString,
+			},
+			F{
+				Name:   "b",
+				TypeIn: Construct(TypeString, MaxLength, 10),
+				TypeOut: Complex{
+					Fields: map[string]T{
+						"f2": TypeString,
+					},
+				},
+			},
+			F{
+				Name: "c",
+				TypeIn: Complex{
+					Fields: map[string]T{
+						"f1": TypeInteger,
+						"f2": TypeString,
+					},
+				},
+				TypeOut: TypeBool,
+			},
+		},
+	}
+	err := c.Invalid()
+	if err == nil {
+		t.Errorf("c must be invalid, but: %s", err)
+	}
+}
